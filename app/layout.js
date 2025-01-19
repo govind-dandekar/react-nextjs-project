0310f3cee1291c6ff6b1e8745779d22e740085c2
Govind Dandekar <govind.dandekar@gmail.com>
2025-01-19 12:08:01 -0800

    add Header component

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,10 @@ export const metadata = {
   description: 'Your first NextJS app!',
 };
 
+// uses standard children prop to inject content
+// layout uses html and body to set up html skeleton
+// <head></head> not rendered here and can be populated
+// by reserved metadata const; children = active page.js file
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
